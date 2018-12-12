44f58df723aa762242a5470f2e5596c327e87942
Maxime Clement <maxime-clement@live.fr>
2018-12-12 18:35:15 +0300

    CV timeline started

--- src/app/pages/cv/cv.module.ts
+++ src/app/pages/cv/cv.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CvComponent } from './cv.component';
+// import { NbThemeModule } from '@nebular/theme';
+import { ThemeModule } from '../../@theme/theme.module';
 
 @NgModule({
   declarations: [
-    CvComponent
+    CvComponent,
   ],
   imports: [
-    CommonModule
-  ]
+    ThemeModule,
+    CommonModule,
+  ],
 })
 export class CvModule { }
